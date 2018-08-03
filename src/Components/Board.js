ddb6320e4b863328a19a30156e3eaad06e32f63f
jendahieu942 <nocrush926@gmail.com>
2018-08-03 09:59:16 +0700

    another calculator Winner

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -32,24 +32,49 @@ class Board extends Component {
             onClick={() => this.handleClick(i)}
         />;
     }
+    // calcWinner(squares) {
+    //     for (let i = 0; i < 5; i++) {
+    //         for (let j = 0; j < 5; j++) {
+    //             let k = j * 5;
+    //             if (squares[k] && squares[k] === squares[k + 1] && squares[k] === squares[k + 2]
+    //                 && squares[k] === squares[k + 3] && squares[k] === squares[k + 4])
+    //                 return squares[k];
+    //         }
+    //         if (squares[i] && squares[i] === squares[i + 5] && squares[i] === squares[i + 10]
+    //             && squares[i] === squares[i + 15] && squares[i] === squares[i + 20])
+    //             return squares[i];
+    //     }
+    //     if (squares[0] && squares[0] === squares[6] && squares[6] === squares[12]
+    //         && squares[12] === squares[18] && squares[18] === squares[24])
+    //         return squares[0];
+    //     if (squares[4] && squares[4] === squares[8] && squares[8] === squares[12]
+    //         && squares[12] === squares[16] && squares[16] === squares[20])
+    //         return squares[4];
+    //     return false;
+    // }
+
     calcWinner(squares) {
-        for (let i = 0; i < 5; i++) {
-            for (let j = 0; j < 5; j++) {
-                let k = j * 5;
-                if (squares[k] && squares[k] === squares[k + 1] && squares[k] === squares[k + 2]
-                    && squares[k] === squares[k + 3] && squares[k] === squares[k + 4])
-                    return squares[k];
-            }
-            if (squares[i] && squares[i] === squares[i + 5] && squares[i] === squares[i + 10]
-                && squares[i] === squares[i + 15] && squares[i] === squares[i + 20])
-                return squares[i];
+        const lines = [
+            [0, 1, 2, 3, 4],
+            [5, 6, 7, 8, 9],
+            [10, 11, 12, 13, 14],
+            [15, 16, 17, 18, 19],
+            [20, 21, 22, 23, 24],
+            [0, 5, 10, 15, 20],
+            [1, 6, 11, 16, 21],
+            [2, 7, 12, 17, 22],
+            [3, 8, 13, 18, 23],
+            [4, 9, 14, 18, 24],
+            [0, 6, 12, 18, 24],
+            [4, 8, 12, 16, 20],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c, d, e] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]
+                && squares[a] === squares[d] && squares[a] === squares[e]){
+                    return squares[a];
+                }
         }
-        if (squares[0] && squares[0] === squares[6] && squares[6] === squares[12]
-            && squares[12] === squares[18] && squares[18] === squares[24])
-            return squares[0];
-        if (squares[4] && squares[4] === squares[8] && squares[8] === squares[12]
-            && squares[12] === squares[16] && squares[16] === squares[20])
-            return squares[4];
         return false;
     }
     render() {
